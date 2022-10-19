b409368f66f4dda4353d78509c95f7e15a139e91
sandykri09 <115703445+sandykri09@users.noreply.github.com>
2022-10-19 15:46:44 +0530

    Add files via upload

---
+++ Data_Types-1.py
@@ -0,0 +1,65 @@
+"""
+Today is Monday 12th Sept 2022
+Topic: Comments and DataTypes
+"""
+
+# print(type(0)) 
+# print(type(2))
+# print(type(6))
+# print(type(5463246326324))
+# print(type(-3323))
+
+# print(type(.0))
+# print(type(0.3))
+# print(type(6.7))
+# print(type(-9.1))
+# print(type(-.3))
+
+# print(type('hello'))
+# print(type("hello"))
+# print(type('''hello'''))
+# print(type("""hello"""))
+
+# print('Hello') 
+# print("Python")
+# print('Hi')
+
+""" 13th sept 2022 Tuesday
+Topic: DataTypes """
+
+''' bool '''
+# print(type(True))
+# print(type(False))
+# print(6<8)
+# print(16<8)
+
+''' list '''
+# print(type([1,'2',3.5,[4,3]]))
+# print(type([]))
+
+''' tuple '''
+# a=1,2,3,4
+# print(a)
+# print(type(a))
+
+''' set '''
+# print(type({1}))
+# print(type({1,2,3}))
+
+''' dict '''
+# print(type({}))
+# print(type({1:'python'}))
+
+''' complex '''
+# a=complex(4)
+# print(a)
+# c=complex(5j)
+# print(c)
+# print(type(3+4j))
+
+''' bytes '''
+# print(type('hello'))
+# print(type(b'hello'))
+
+''' bytearray'''
+# print((type(bytearray(b'hello'))))
