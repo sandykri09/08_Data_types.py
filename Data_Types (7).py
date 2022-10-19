48ffc478e05a2712c03d2d1263a344756a57bbaa
sandykri09 <115703445+sandykri09@users.noreply.github.com>
2022-10-19 15:47:12 +0530

    Add files via upload

---
+++ Data_Types (7).py
@@ -0,0 +1,25 @@
+"""
+Today is Monday 12th Sept 2022
+Topic: Comments and DataTypes
+"""
+
+# print(type(0)) 
+# print(type(2))
+# print(type(6))
+# print(type(5463246326324))
+# print(type(-3323))
+
+# print(type(.0))
+# print(type(0.3))
+# print(type(6.7))
+# print(type(-9.1))
+# print(type(-.3))
+
+# print(type('hello'))
+# print(type("hello"))
+# print(type('''hello'''))
+# print(type("""hello"""))
+
+# print('Hello') 
+# print("Python")
+# print('Hi')
